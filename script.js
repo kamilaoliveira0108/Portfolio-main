3b4da05aa20a473711cbb88c4782b496aa373ef5
kamilaoliveira0108 <kamyyumy300@gmail.com>
2024-05-19 14:13:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal(".about-me", {duration: 1500})
+sr.reveal(".skills", {duration: 1500})
+sr.reveal(".projetos", {duration: 1500})
+sr.reveal(".contact", {duration: 1500})
